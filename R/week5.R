9a05e3356fc9bcc8ec6dec2f0c08d6e79b2cd6c5
dtomeh <tomeh001@umn.edu>
2020-02-18 14:33:04 -0600

    Adata_tbl cleaning

--- R/week5.R
+++ R/week5.R
@@ -6,5 +6,12 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 Adata_tbl <- read_delim("../Data/Aparticipants.dat", delim ="-", col_names= c("casenum", "parnum", "stimver", "datadate", "qs"))
 Anotes_tbl <- read_csv("../Data/Anotes.csv")
-Bdata_tbl <- read_delim("../Data/Bparticipants.dat", delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"))
+Bdata_tbl <- read_delim("../Data/Bparticipants.dat", delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", paste0(c("q"), 1:10)))
 Bnotes_tbl <- read_delim("../data/Bnotes.txt", delim = "\t", col_names = T)
+
+#Data Cleaning
+library(lubridate)
+Adata_tbl <- separate(Adata_tbl, qs, paste0(c("q"), 1:5)) %>%  
+  mutate_at(vars(5:9), as.numeric) %>%
+  mutate_at(vars(datadate), mdy_hms)
+
