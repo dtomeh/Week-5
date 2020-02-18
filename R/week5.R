427b754f5daccbab0c2a76b7deb513c5b632c3ae
dtomeh <tomeh001@umn.edu>
2020-02-18 14:38:13 -0600

    Aaggr_tbl % Baggr_table + Joins

--- R/week5.R
+++ R/week5.R
@@ -15,3 +15,14 @@ Adata_tbl <- separate(Adata_tbl, qs, paste0(c("q"), 1:5)) %>%
   mutate_at(vars(5:9), as.numeric) %>%
   mutate_at(vars(datadate), mdy_hms)
 
+Aaggr_tbl <- select(Adata_tbl, parnum, q1:q5) %>% 
+  group_by(parnum) %>% 
+  summarise_at(vars(q1:q5), mean)
+Baggr_tbl <- select(Bdata_tbl, parnum, q1:q10) %>% 
+  group_by(parnum) %>%
+  summarise_at(vars(q1:q10), mean)
+Aaggr_tbl <-Aaggr_tbl %>% inner_join(Anotes_tbl, by="parnum")
+Baggr_tbl <-Baggr_tbl %>% inner_join(Bnotes_tbl, by="parnum")
+bind_rows(Aaggr_tbl, Baggr_tbl, .id = "datafile") %>% 
+  filter(is.na(notes)) %>%
+  count(datafile)
